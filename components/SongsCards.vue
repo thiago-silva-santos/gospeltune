bd9331e938918c9b2086135625f49f1b962c0d30
thiago-silva-santos <thiagomj98@gmail.com>
2023-04-01 21:16:19 -0300

    Desabilita cards com músicas sem cifra

--- components/SongsCards.vue
+++ components/SongsCards.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
     <section class=" py-10" v-if="hasItems">
-      <div
-        class="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div class="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         <template v-for="item in searchResults" :key="item">
-            <nuxt-link :to="item.tipo == 'corinho' ? `/song-view/corinhos/${item.id}` : `/song-view/hinos-harpa-crista/${item.id}`">
-            <div
-              class="card_ ">
+          <nuxt-link v-if="item.cifra.length > 0"
+            :to="item.tipo == 'corinho' ? `/song-view/corinhos/${item.id}` : `/song-view/hinos-harpa-crista/${item.id}`">
+            <div class="card_ ">
               <div class="song_number text-center font-bold text-lg">
                 {{ item.id }}
               </div>
@@ -15,6 +14,17 @@
               </div>
             </div>
           </nuxt-link>
+          <div v-else class="card_">
+            <div class="song_number text-center font-bold text-lg">
+              {{ item.id }}
+            </div>
+            <div class="song_title text-center">
+              {{ item.nome }}
+            </div>
+            <span class="material-symbols-outlined" id="empty_song">
+              sentiment_dissatisfied
+            </span>
+          </div>
         </template>
       </div>
     </section>
@@ -58,7 +68,7 @@ export default {
   },
   watch: {
     searchResults(value) {
-      if(value.length == 0) {
+      if (value.length == 0) {
         this.hasItems = false
       }
       else {
@@ -69,18 +79,24 @@ export default {
 };
 </script>
 <style lang="css" scoped>
-
 .card_ {
-  @apply w-full flex flex-col gap-4 lg:p-4 md:p-4 sm:p-2 p-2 rounded shadow-lg bg-white cursor-pointer;
+  @apply w-full flex flex-col gap-4 lg:p-4 md:p-4 sm:p-2 p-2 rounded-lg shadow-lg bg-white cursor-pointer relative;
   transition: all ease-in-out .3s;
 }
 
 .no-results {
   @apply flex items-center w-full h-20 py-6 px-2 text-slate-600 text-lg
 }
-@media (min-width: 500px) {
-  .card_:hover{
-  @apply bg-gray-100 text-black
+#empty_song {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  @apply w-6 h-6 text-slate-300
 }
+
+@media (min-width: 500px) {
+  .card_:hover {
+    @apply bg-gray-100 text-black
+  }
 }
 </style>
