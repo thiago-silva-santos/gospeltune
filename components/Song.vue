750eb98fe35cc6d89c34eae6dc13b6d9d6eab2be
Thiago Silva <thiago.dev10@gmail.com>
2023-04-05 12:28:35 -0300

    Ajustes na função que divide a cifra

--- components/Song.vue
+++ components/Song.vue
@@ -2,31 +2,31 @@
      <section class="flex pt-10">
 
           <pre class="flex flex-col">
-               <template v-for="linha in verificacao" :key="linha">
-                         <template v-if="linha.verse">
-                              <span class="flex" >
-                                 <template v-for="item in linha.chords">
-                                         <b class="w-[0.450rem]">{{ tom[item]?.notacao }}</b>
-                                   </template>
-                                      </span>    
-                             <span class="linha-verso">{{ linha.verse }}</span>
-                         </template>                         
-                        <div v-else class="h-10">
-                         </div>
-                  </template>
-                  <template v-if="divideCifra.length >= 1" v-for="linha in divideCifra[1]" :key="linha">
-                         <template v-if="linha.verse">
-                              <span class="flex" >
-                                 <template v-for="item in linha.chords">
-                                         <b class="w-[0.450rem]">{{ tom[item]?.notacao }}</b>
-                                   </template>
-                                      </span>    
-                             <span class="linha-verso font-bold">{{ linha.verse }}</span>
-                         </template>                         
-                        <div v-else class="h-10">
-                         </div>
-                  </template>
-          </pre>
+                    <template v-for="linha in cifraDividida[0]" :key="linha">
+                              <template v-if="linha.verse">
+                                   <span class="flex" >
+                                      <template v-for="item in linha.chords">
+                                              <b class="w-[0.450rem]">{{ tom[item]?.notacao }}</b>
+                                        </template>
+                                           </span>    
+                                  <span class="linha-verso">{{ linha.verse }}</span>
+                              </template>                         
+                             <div v-else class="h-10">
+                              </div>
+                       </template>
+                       <template v-for="linha in cifraDividida[1]" :key="linha">
+                              <template v-if="linha.verse">
+                                   <span class="flex" >
+                                      <template v-for="item in linha.chords">
+                                              <b class="w-[0.450rem]">{{ tom[item]?.notacao }}</b>
+                                        </template>
+                                           </span>    
+                                  <span class="linha-verso font-bold">{{ linha.verse }}</span>
+                              </template>                         
+                             <div v-else class="h-10">
+                              </div>
+                       </template>
+               </pre>
 
      </section>
 </template>
@@ -57,32 +57,20 @@ export default {
 
                return this.campo[this.tonalidade].acordes;
           },
-          divideCifra() {
-               //CRIAR LOGICA PRA SEPARAR REFRÃO
-
+          cifraDividida() {
                const index = this.song.findIndex(obj => obj.verse === '[ Refrão ]');
                if (index === -1) {
-                    // Se não encontrar nenhum objeto com a string específica, retorna o array inteiro
-                    return this.song;
+                    return [this.song]
                } else {
-                    // Divide o array em duas partes com base no índice encontrado
                     const parte1 = this.song.slice(0, index);
                     const parte2 = this.song.slice(index);
                     return [parte1, parte2];
                }
 
           },
-          verificacao() {
-               if(this.divideCifra.every(elem => Array.isArray(elem))) {
-                    return this.divideCifra[0]
-               } else {
-                    return this.song
-               }
-          }
      },
 
      created() {
-          console.log(this.divideCifra)
      }
 }
 </script>
@@ -92,16 +80,8 @@ export default {
      letter-spacing: 1px;
 }
 
-.chord {
-     height: 24px;
-     @apply text-red-600;
-}
-
 b {
      @apply text-red-600
 }
 
-/* b:not(:empty) {
-     @apply w-fit;
-} */
 </style>
