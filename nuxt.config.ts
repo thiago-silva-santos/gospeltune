f1e7b5eb907e1a42103e75c9c116f485f1c8be43
Thiago Silva <thiago.dev10@gmail.com>
2023-04-13 17:03:59 -0300

    implementa logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
         { name: 'format-detection', content: 'telephone=no' }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/x-icon', href: '/gospeltunelogo.png' },
         { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Baloo+Da+2:wght@400;500;600;700;800&display=swap" },
         { rel: 'stylesheet', href: "https://fonts.googleapis.com/icon?family=Material+Icons" },
         { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" }
